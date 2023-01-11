11f723f58d6cdab31e327e0148123779dd50073b
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-11 17:52:43 +0100

    setting up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,24 @@
-{
-    "env": {
-      "browser": true,
-      "es2021": true
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: { jest: true },
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-      {
-        "files": ["**/*.test.js"],
-        "env": { "jest": true },
-        "plugins": ["jest"],
-        "extends": ["plugin:jest/recommended"],
-        "rules": { "jest/prefer-expect-assertions": "off" }
-      }
-    ],
-    "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
-    },
-    "rules": {}
-  }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": "off",
+  },
+};
