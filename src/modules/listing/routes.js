cc5b169998a659dcf790366e74ec255a3ead87f2
Zycrix <Doffa98@live.no>
2023-01-12 20:29:31 +0100

    Adding starting verion of post function. More updates comming soon

--- src/modules/listing/routes.js
+++ src/modules/listing/routes.js
@@ -7,3 +7,25 @@ export const listingsRouter = express.Router();
 listingsRouter.get("/", async (req, res) => {
   res.send("This is the listing request");
 });
+
+listingsRouter.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    const result = await databasePrisma.listing.create({
+      data: {
+        title: req.body.title,
+        tags: req.body.tags,
+        description: req.body.description,
+        requirements: req.body.requirements,
+        deadline: req.body.deadline,
+        updated: new Date(),
+        authorId: req.body.authorId,
+      },
+    });
+
+    res.status(200).json(result);
+  } catch (e) {
+    console.log(e);
+    res.json({ error: e });
+  }
+});
