37420722405f005c6f80989ef109161f0e59918a
Ole M <olesnoen@gmail.com>
2023-01-12 23:26:39 +0100

    get requests

--- src/modules/application/routes.js
+++ src/modules/application/routes.js
@@ -3,7 +3,32 @@ import { databasePrisma } from "../../prismaClient.js";
 
 export const applicationsRouter = express.Router();
 
-// Handling request using router
-applicationsRouter.get("/", async (req, res) => {
-  res.send("This is the app request");
-});
+applicationsRouter
+    .get("/", async (req, res) => {
+        try {
+            const applications = await databasePrisma.application.findMany({
+                include: { Offers: true },
+            });
+            res.status(200).json(applications);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: `internal server error`, code: "500" });
+        }
+    })
+    .get("/:id", async (req, res) => {
+        try {
+            const URLid = req.params.id;
+            const applicationById = await databasePrisma.application.findUnique({
+                where: {
+                    id: URLid,
+                },
+            });
+            if (applicationById === null) {
+                res.status(404).json({ message: `no application found with id "${URLid}"` });
+            }
+            res.json(applicationById);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: `internal server error`, code: "500" });
+        }
+    });
