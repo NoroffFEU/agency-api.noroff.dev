95b05d209794f261facce781887fd491196e5925
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-12 18:34:14 +0100

    added password hashing to login and register route

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -1,22 +1,25 @@
 import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
-
+import { findUserByEmail } from "../../utilities/findUser.js";
+import { verifyPassword, generateHash } from "../../utilities/password.js";
+import { signToken } from "../../utilities/jsonWebToken.js";
+import { verifyToken } from "../../utilities/jsonWebToken.js";
 export const usersRouter = express.Router();
 
 // POST /users
 usersRouter.post("/", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+    const { hash, salt } = await generateHash(password);
     const result = await databasePrisma.user.create({
       data: {
         firstName,
         lastName,
         email,
-        password,
-        salt: "salty",
+        password: hash,
+        salt,
       },
     });
-
     res.json(result);
   } catch (err) {
     console.log(err);
@@ -25,7 +28,41 @@ usersRouter.post("/", async (req, res) => {
 });
 
 //  POST /users/login
-usersRouter.post("/login", async (req, res) => {});
+usersRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const profile = await findUserByEmail(email);
+
+    if (!profile) {
+      throw new Error("Invalid email or password");
+    }
+
+    const correctPassword = await verifyPassword(profile, password);
+    console.log(correctPassword);
+
+    if (!correctPassword) {
+      throw new Error("Invalid email or password");
+    }
+
+    let token;
+    //Creating jwt token
+    token = signToken(profile);
+
+    res.status(200).json({
+      ok: true,
+      data: {
+        userId: profile.id,
+        firstName: profile.firstName,
+        lastName: profile.lastName,
+        email: profile.email,
+        password: profile.password,
+        token: token,
+      },
+    });
+  } catch (err) {
+    res.status(401).json(err.message);
+  }
+});
 
 // GET /users
 usersRouter.get("/", async (req, res) => {
