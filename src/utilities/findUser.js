8296bca5f5e2a4dd1faa9ab4b49317675497107f
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-12 22:38:11 +0100

    added docs to find user

--- src/utilities/findUser.js
+++ src/utilities/findUser.js
@@ -1,5 +1,10 @@
 import { databasePrisma } from "../prismaClient.js";
 
+/**
+ * Finds and returns a user based on email
+ * @param {String} email
+ * @returns {Object} user data
+ */
 export async function findUserByEmail(email) {
   return await databasePrisma.user.findUnique({
     where: {
@@ -8,6 +13,11 @@ export async function findUserByEmail(email) {
   });
 }
 
+/**
+ * Finds and returns a user based on id
+ * @param {String} id
+ * @returns {Object} user data
+ */
 export async function findUserById(id) {
   return await databasePrisma.user.findUnique({
     where: {
