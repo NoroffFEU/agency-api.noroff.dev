3a6d6a81a716e9524597f80595c9ab07682da2f8
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-12 22:37:06 +0100

    salt removed from schema, and password function updated

--- src/utilities/password.js
+++ src/utilities/password.js
@@ -1,13 +1,24 @@
 import bcrypt from "bcrypt";
 
+/**
+ * Compares plain text password to hashed password
+ * @param {Object} profile users profile with hashed password
+ * @param {String} password plain text string provided in request
+ * @returns {Boolean} true/false
+ */
 export async function verifyPassword(profile, password) {
   return bcrypt.compareSync(password, profile.password);
 }
 
+/**
+ * Creates a password hash, that has been sa
+ * @param {String} password Plain text password
+ * @returns {Object} hash
+ */
 export async function generateHash(password) {
   const saltRounds = 10;
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = await bcrypt.hash(password, salt);
 
-  return { hash, salt };
+  return hash;
 }
