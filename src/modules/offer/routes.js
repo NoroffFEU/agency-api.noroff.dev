1415ee2345be74ba80bc1b09f84203a6e4de70b6
pnordboj <91888307+pnordboj@users.noreply.github.com>
2023-01-13 18:34:25 +0100

    Added GET /:id

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -7,3 +7,56 @@ export const offersRouter = express.Router();
 offersRouter.get("/", async (req, res) => {
   res.send("This is the offers request");
 });
+
+offersRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const offer = await databasePrisma.offer.findUnique({
+      where: {
+        id: String(id),
+      },
+    });
+    if (!offer) {
+      res.status(404).json({ message: "Offer not found" });
+      return;
+    }
+    res.send(offer);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: `${error}` });
+  }
+});
+
+offersRouter.post("/", async (req, res) => {
+  const { title, description, price, userId } = req.body;
+  const offer = await databasePrisma.offer.create({
+    data: {
+      userId,
+    },
+  });
+  res.send(offer);
+});
+
+offersRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { state } = req.body;
+  const offer = await databasePrisma.offer.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      state,
+    },
+  });
+  res.send(offer);
+});
+
+offersRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const offer = await databasePrisma.offer.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.send(offer);
+});
