2cdddb06ab79d9f88ae5bf6e7aaef7c1bd3ce89c
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-01-13 19:28:25 +0100

    made create offer function in src/modules/offer/router.js

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -7,3 +7,32 @@ export const offersRouter = express.Router();
 offersRouter.get("/", async (req, res) => {
   res.send("This is the offers request");
 });
+
+offersRouter.post("/offers/create", async (req, res) => {
+  try {
+    const {
+      listingId,
+      applicationId,
+      userId,
+      applicantId,
+      userRole,
+      offerState,
+    } = req.body;
+
+    const offer = await databasePrisma.offer.create({
+      data: {
+        listingId: listingId,
+        applicationId: applicationId,
+        userId: userId,
+        applicantId: applicantId,
+        applicant: userRole,
+        state: offerState,
+      },
+    });
+
+    res.status(200).json(offer);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
+});
