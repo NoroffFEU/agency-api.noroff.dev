c93cf41bc48a79dc8b9aa13d2f35607af713e2a4
andjhay <88853764+andjhay@users.noreply.github.com>
2023-01-14 13:52:36 +0100

    .send to .json added more error and stop id update

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -65,58 +65,69 @@ usersRouter.put("/:id", async (req, res) => {
   const user = await findUserById(id);
   //Throw 404 if user doesn't exist
   if (!user) {
-    res.status(400).send("User not found");
+    res.status(400).json("User not found");
     return;
   }
   //Make sure user has token and removes Bearer if need be
   const token = req.headers.authorization;
   var readyToken = token;
   if (!token) {
-    res.status(401).send("User has to be authenticated to make this request");
+    res.status(401).json("User has to be authenticated to make this request");
   } else if (token.includes("Bearer")) {
     readyToken = token.slice(7);
   }
 
   if (readyToken) {
-    var verified = verifyToken(readyToken);
-    var tokenUser = decodeToken(readyToken);
+    try {
+      var verified = verifyToken(readyToken);
+      var tokenUser = decodeToken(readyToken);
+    } catch (error) {
+      res.status(401).json(error);
+    }
   }
-
   //Throw 401 error if user isn't the correct user
   if (verified.userId != id || tokenUser.userId != id) {
-    res.status(401).send("User does not match user to be edited");
+    res.status(401).json("User does not match user to be edited");
   } else {
     try {
+      // removes id from body stopping user from updating it
+      let idMsg = "";
+      if ("id" in req.body == true) {
+        delete req.body.id;
+        idMsg = "An ID was found in the body and was removed pre update";
+      }
       // Updates the user
       const result = await databasePrisma.user.update({
         where: { id },
         data: req.body,
       });
+      result.error = idMsg;
       res.status(200).json(result);
     } catch (error) {
-      console.log(error.message);
       if (!error.status) {
+        // Checks for database related errors
         if (error.meta != undefined) {
           res
             .status(409)
-            .send(
-              `The input ${error.meta.target[0]} already exists for another user` +
+            .json(
+              `The unique input ${error.meta.target[0]} already exists for another user` +
                 " " +
                 error
             );
         } else {
           res
             .status(400)
-            .send(
-              "Argument value error it does not exist see details" +
+            .json(
+              "Argument or input value does not exist see details in this message regarding the value that is at fault" +
+                " " +
                 error.message
             );
         }
       } else {
         if (error.status) {
-          res.status(error.status).send(error);
+          res.status(error.status).json(error);
         } else {
-          res.status(500).send("Internal server error.");
+          res.status(500).json("Internal server error.");
         }
       }
     }
