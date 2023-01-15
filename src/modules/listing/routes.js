d4b8b342cda02def9a202064ae40eb706095f597
Viorel Lazari <vlazari@gmail.com>
2023-01-15 16:58:05 +0100

    Listing DELETE with middleware function

--- src/modules/listing/routes.js
+++ src/modules/listing/routes.js
@@ -5,5 +5,48 @@ export const listingsRouter = express.Router();
 
 // Handling request using router
 listingsRouter.get("/", async (req, res) => {
-  res.send("This is the listing request");
+	const list = await databasePrisma.listing.findMany();
+	res.send(list);
 });
+
+/**
+ * DELETE listing endpoint.
+ */
+listingsRouter.delete("/:id", getListing, async (req, res) => {
+	try {
+		await databasePrisma.listing.delete({
+			where: {
+				id: req.params.id,
+			},
+		});
+		res.status(200).json({ message: `Listing with id: ${req.params.id} was deleted.` });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+/**
+ * Middleware function that checks if listing with specified id does exist.
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @returns "status 404" with error message if listing doesn't exist.
+ */
+async function getListing(req, res, next) {
+	let listing;
+	try {
+		listing = await databasePrisma.listing.findUnique({
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (listing == null) {
+			return res.status(404).json({ message: `Listing with id ${req.params.id} doesn't exist.` });
+		}
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+
+	res.listing = listing;
+	next();
+}
--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,2 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 export const databasePrisma = new PrismaClient();
+
+// async function ab1() {
+// 	await databasePrisma.listing.create({
+// 		data: {
+// 			title: "title3",
+// 			tags: ["1", "2"],
+// 			description: "description2",
+// 			requirements: ["HTML", "CSS"],
+// 			deadline: new Date(2023, 11, 18),
+// 			created: new Date(),
+// 			updated: new Date(),
+// 			authorId: "1",
+// 		},
+// 	});
+// }
+// ab1();
