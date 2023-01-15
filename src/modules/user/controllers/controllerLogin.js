c93e424722c10741e9695cd416ab2e951e2d07b9
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-15 22:15:27 +0100

    Updated error reporting added email/password validation

--- src/modules/user/controllers/controllerLogin.js
+++ src/modules/user/controllers/controllerLogin.js
@@ -1,7 +1,6 @@
 import { signToken } from "../../../utilities/jsonWebToken.js";
 import { verifyPassword } from "../../../utilities/password.js";
 import { findUserByEmail } from "../../../utilities/findUser.js";
-import { createThrownError } from "../../../utilities/errorMessages.js";
 
 /**
  * validates request body, signs jwt token and returns response object
@@ -10,35 +9,24 @@ import { createThrownError } from "../../../utilities/errorMessages.js";
  */
 export const handleLogin = async function (req) {
   const { email, password } = req.body;
-  if (!password || !email) {
-    throw createThrownError(
-      400,
-      `Bad request: request body missing${!password ? " password" : ""}${
-        !password && !email ? " and" : ""
-      }${!email ? " email" : ""}.`
-    );
-  }
-
   const profile = await findUserByEmail(email);
 
-  //could return email doesn't exist here?
+  // verify email exists
   if (!profile) {
-    throw createThrownError(403, `Invalid email or password.`);
+    return { status: 403, data: { message: `Invalid email or password.` } };
   }
 
+  // verify password
   const correctPassword = await verifyPassword(profile, password);
-  console.log(correctPassword);
-
   if (!correctPassword) {
-    throw createThrownError(403, `Invalid email or password.`);
+    return { status: 403, data: { message: `Invalid email or password.` } };
   }
 
-  let token;
   //Creating jwt token
-  token = signToken(profile);
+  const token = signToken(profile);
 
   return {
-    ok: true,
+    status: 200,
     data: {
       userId: profile.id,
       firstName: profile.firstName,
