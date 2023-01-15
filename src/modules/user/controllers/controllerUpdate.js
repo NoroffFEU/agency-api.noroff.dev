c25925e335c965049695f4e6e34e9b9bfe8617aa
andjhay <88853764+andjhay@users.noreply.github.com>
2023-01-15 13:15:27 +0100

    Conformed to standard format + added pass hash

---
+++ src/modules/user/controllers/controllerUpdate.js
@@ -0,0 +1,93 @@
+import { generateHash } from "../../../utilities/password.js";
+import { findUserById } from "../../../utilities/findUser.js";
+import {
+  decodeToken,
+  signToken,
+  verifyToken,
+} from "../../../utilities/jsonWebToken.js";
+import { databasePrisma } from "../../../prismaClient.js";
+import { createThrownError } from "../../../utilities/errorMessages.js";
+
+/**
+ * validates request body, signs jwt token and returns response object
+ * @param {Object} req API Request
+ */
+export const handleUpdate = async function (req) {
+  // Find the user to be updated
+  const id = req.params.id;
+  const user = await findUserById(id);
+  const jwt = signToken(user);
+  console.log(jwt);
+  //Throw 404 if user doesn't exist
+  if (!user) {
+    throw createThrownError(404, `User not found`);
+  }
+  //Make sure user has token and removes Bearer if need be
+  const token = req.headers.authorization;
+  var readyToken = token;
+  if (!token) {
+    throw createThrownError(
+      401,
+      `User has to be authenticated to make this request`
+    );
+  } else if (token.includes("Bearer")) {
+    readyToken = token.slice(7);
+  }
+
+  if (readyToken != undefined) {
+    try {
+      var verified = verifyToken(readyToken);
+      var tokenUser = decodeToken(readyToken);
+    } catch (error) {
+      throw createThrownError(401, `Auth token not valid.`);
+    }
+  }
+  //Throw 401 error if user isn't the correct user
+  if (verified.userId != id || tokenUser.userId != id) {
+    throw createThrownError(401, `User does not match user to be edited`);
+  } else {
+    try {
+      // removes id from body stopping user from updating it
+      let idMsg = "User details updated successfully.";
+      if ("id" in req.body == true) {
+        delete req.body.id;
+        idMsg = "An ID was found in the body and was removed pre update.";
+      }
+      let newPass;
+      console.log(req.body);
+      if ("password" in req.body == true) {
+        newPass = req.body.password;
+        const hash = await generateHash(newPass);
+        console.log(hash);
+        req.body.password = hash;
+      }
+      console.log(req.body);
+      // Updates the user
+      const result = await databasePrisma.user.update({
+        where: { id },
+        data: req.body,
+      });
+      result.response = idMsg;
+      return result;
+    } catch (error) {
+      if (!error.status) {
+        // Checks for database related errors
+        if (error.meta != undefined) {
+          throw createThrownError(
+            409,
+            `The unique input ${error.meta.target[0]} already exists for another user`
+          );
+        } else {
+          throw createThrownError(
+            400,
+            `An argument or input value does not exist or cannot be edited in the database ${error.message}`
+          );
+        }
+      } else {
+        if (error.status) {
+          throw createThrownError(error.status, error.message);
+        }
+      }
+    }
+  }
+};
--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -2,12 +2,7 @@ import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
 import { generateHash } from "../../utilities/password.js";
 import { handleLogin } from "./controllers/controllerLogin.js";
-import { findUserById } from "../../utilities/findUser.js";
-import {
-  decodeToken,
-  signToken,
-  verifyToken,
-} from "../../utilities/jsonWebToken.js";
+import { handleUpdate } from "./controllers/controllerUpdate.js";
 
 export const usersRouter = express.Router();
 
@@ -60,76 +55,15 @@ usersRouter.get("/:id", async (req, res) => {});
 
 // PUT /users/:id
 usersRouter.put("/:id", async (req, res) => {
-  //Finds the user
-  const id = req.params.id;
-  const user = await findUserById(id);
-  //Throw 404 if user doesn't exist
-  if (!user) {
-    res.status(400).json("User not found");
-    return;
-  }
-  //Make sure user has token and removes Bearer if need be
-  const token = req.headers.authorization;
-  var readyToken = token;
-  if (!token) {
-    res.status(401).json("User has to be authenticated to make this request");
-  } else if (token.includes("Bearer")) {
-    readyToken = token.slice(7);
-  }
-
-  if (readyToken) {
-    try {
-      var verified = verifyToken(readyToken);
-      var tokenUser = decodeToken(readyToken);
-    } catch (error) {
-      res.status(401).json(error);
-    }
-  }
-  //Throw 401 error if user isn't the correct user
-  if (verified.userId != id || tokenUser.userId != id) {
-    res.status(401).json("User does not match user to be edited");
-  } else {
-    try {
-      // removes id from body stopping user from updating it
-      let idMsg = "";
-      if ("id" in req.body == true) {
-        delete req.body.id;
-        idMsg = "An ID was found in the body and was removed pre update";
-      }
-      // Updates the user
-      const result = await databasePrisma.user.update({
-        where: { id },
-        data: req.body,
-      });
-      result.error = idMsg;
-      res.status(200).json(result);
-    } catch (error) {
-      if (!error.status) {
-        // Checks for database related errors
-        if (error.meta != undefined) {
-          res
-            .status(409)
-            .json(
-              `The unique input ${error.meta.target[0]} already exists for another user` +
-                " " +
-                error
-            );
-        } else {
-          res
-            .status(400)
-            .json(
-              "Argument or input value does not exist see details in this message regarding the value that is at fault" +
-                " " +
-                error.message
-            );
-        }
-      } else {
-        if (error.status) {
-          res.status(error.status).json(error);
-        } else {
-          res.status(500).json("Internal server error.");
-        }
-      }
+  try {
+    const data = await handleUpdate(req);
+    res.status(200).json(data);
+  } catch (err) {
+    const errorObject = await JSON.parse(err.message);
+    if (errorObject.status) {
+      res.status(errorObject.status).json(errorObject.message);
+    } else {
+      res.status(500).json("Internal server error.");
     }
   }
 });
