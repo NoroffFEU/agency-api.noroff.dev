04ad98b8c6f97ae6f9d8e943fe09557bb86bf3d6
olee2 <oleaeik@protonmail.com>
2023-01-16 19:02:51 +0100

    Updating tests

--- src/modules/user/routes.test.js
+++ src/modules/user/routes.test.js
@@ -2,7 +2,8 @@ import request from "supertest";
 
 const baseURL = "http://localhost:3000"
 
-let user;
+let createdUser;
+let loggedInUser;
 
 const testUser = {
     "firstName": "John",
@@ -12,16 +13,25 @@ const testUser = {
 }
 
 describe("POST /users", () => {
-    it("should create a new user and return 201 response", async () => {
+    it("should create and return new user object and a 201 response", async () => {
         const response = await request(baseURL).post("/users").send(testUser);
-        user = response.body;
+        createdUser = response.body;
         expect(response.body.email).toEqual(testUser.email);
         expect(response.statusCode).toBe(201);
     })
 })
 
+describe("POST /users(login", () => {
+    it("should return an object with a token and 200 response", async () => {
+        const response = await request(baseURL).post("/users/login").send({ "email": testUser.email, "password": testUser.password });
+        loggedInUser = response.body;
+        expect(response.body.firstName).toEqual(testUser.firstName);
+        expect(response.statusCode).toBe(200);
+    })
+})
+
 describe("GET /users", () => {
-    it("should return a array og users and a 200 response", async () => {
+    it("should return a array of users and a 200 response", async () => {
         const response = await request(baseURL).get("/users");
         expect(response.body[0].name).toEqual(testUser.name);
         expect(response.statusCode).toBe(200);
@@ -30,7 +40,7 @@ describe("GET /users", () => {
 
 describe("GET /users/:id", () => {
     it("should return a user object and a 200 response", async () => {
-        const { id } = user;
+        const { id } = createdUser;
         const response = await request(baseURL).get(`/users/${id}`);
         expect(response.body.name).toEqual(testUser.name);
         expect(response.statusCode).toBe(200);
@@ -39,7 +49,7 @@ describe("GET /users/:id", () => {
 
 describe("PUT /users/:id", () => {
     it("should return a updated user object and a 200 response", async () => {
-        const { id } = user;
+        const { id } = createdUser;
         const data = { firstName: "Lesslie" }
         const response = await request(baseURL).put(`/users/${id}`).send(data);
         expect(response.body.firstName).toEqual(data.firstName);
@@ -49,7 +59,7 @@ describe("PUT /users/:id", () => {
 
 describe("DELETE /users/:id", () => {
     it("should return 200", async () => {
-        const { id } = user;
+        const { id } = createdUser;
         const response = await request(baseURL).delete(`/users/${id}`);
         expect(response.statusCode).toBe(200);
     })
