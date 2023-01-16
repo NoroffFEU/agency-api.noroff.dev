fbba4aeecbbcbbc5ecb9d6b5da2826f0e1e99a33
Viorel Lazari <vlazari@gmail.com>
2023-01-16 22:48:10 +0100

    Move middleware function into middlware folder

--- src/modules/listing/routes.js
+++ src/modules/listing/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
+import { checkIfIdExist } from "./middleware/index.js";
 
 export const listingsRouter = express.Router();
 
@@ -12,7 +13,7 @@ listingsRouter.get("/", async (req, res) => {
 /**
  * DELETE listing endpoint.
  */
-listingsRouter.delete("/:id", getListing, async (req, res) => {
+listingsRouter.delete("/:id", checkIfIdExist, async (req, res) => {
 	try {
 		await databasePrisma.listing.delete({
 			where: {
@@ -24,29 +25,3 @@ listingsRouter.delete("/:id", getListing, async (req, res) => {
 		res.status(500).json({ message: err.message });
 	}
 });
-
-/**
- * Middleware function that checks if listing with specified id does exist.
- * @param {*} req
- * @param {*} res
- * @param {*} next
- * @returns "status 404" with error message if listing doesn't exist.
- */
-async function getListing(req, res, next) {
-	let listing;
-	try {
-		listing = await databasePrisma.listing.findUnique({
-			where: {
-				id: req.params.id,
-			},
-		});
-		if (listing == null) {
-			return res.status(404).json({ message: `Listing with id ${req.params.id} doesn't exist.` });
-		}
-	} catch (err) {
-		return res.status(500).json({ message: err.message });
-	}
-
-	res.listing = listing;
-	next();
-}
