6f0270ae11ac56bd4873a6487d69529211631bbe
andjhay <88853764+andjhay@users.noreply.github.com>
2023-01-16 18:35:33 +0100

    added only admin can edit roles and any user

--- src/modules/user/controllers/controllerUpdate.js
+++ src/modules/user/controllers/controllerUpdate.js
@@ -36,93 +36,104 @@ export const handleUpdate = async function (req) {
     }
   }
   //Throw 401 error if user isn't the correct user
-  if (verified.userId != id || tokenUser.userId != id) {
-    return Promise.resolve({
-      status: 401,
-      message: "User does not match user to be edited",
-    });
-  } else {
-    // removes id from body stopping user from updating it
-    let idMsg = "User details updated successfully.";
-    if ("id" in req.body == true) {
-      delete req.body.id;
-      idMsg = "An ID was found in the body and was removed pre update.";
+  if (user.role != "Admin") {
+    if (verified.userId != id || tokenUser.userId != id) {
+      return Promise.resolve({
+        status: 401,
+        message: "User does not match user to be edited",
+      });
     }
+  }
+  // removes id from body stopping user from updating it
+  let idMsg = "User details updated successfully.";
+  if ("id" in req.body == true) {
+    delete req.body.id;
+    idMsg += " Argument id was found in request body and was removed.";
+  }
 
-    // Handles password changes
-    if (
-      "password" in req.body == true &&
-      req.body.currentpassword != undefined
-    ) {
-      if (req.body.password.length >= 5 && req.body.password.length <= 20) {
-        if ((await verifyPassword(user, req.body.currentpassword)) == true) {
-          delete req.body.currentpassword;
-          const hash = await generateHash(req.body.password);
-          req.body.password = hash;
-        } else {
-          return Promise.resolve({
-            status: 401,
-            message: "Incorrect Password",
-          });
-        }
+  // removes createdAt from body to stop user created date being edited
+  if ("createdAt" in req.body == true) {
+    delete req.body.createdAt;
+    idMsg += " Argument createdAt was found in request body and was removed.";
+  }
+
+  // if user is a applicant or client don't allow an update of role
+  if (user.role == "Applicant" || user.role == "Client") {
+    delete req.body.role;
+    idMsg +=
+      " Argument role was found in request body and was removed as user doesn't have authority to update this.";
+  }
+
+  // Handles password changes
+  if ("password" in req.body == true && req.body.currentpassword != undefined) {
+    if (req.body.password.length >= 5 && req.body.password.length <= 20) {
+      if ((await verifyPassword(user, req.body.currentpassword)) == true) {
+        delete req.body.currentpassword;
+        const hash = await generateHash(req.body.password);
+        req.body.password = hash;
       } else {
         return Promise.resolve({
           status: 401,
-          message:
-            "Password does not meet required parameters length: min 5, max 20",
+          message: "Incorrect Password",
         });
       }
-    } else if (
-      "password" in req.body == true &&
-      req.body.currentpassword == undefined
-    ) {
+    } else {
       return Promise.resolve({
         status: 401,
-        message: "No current password provided",
+        message:
+          "Password does not meet required parameters length: min 5, max 20",
       });
     }
+  } else if (
+    "password" in req.body == true &&
+    req.body.currentpassword == undefined
+  ) {
+    return Promise.resolve({
+      status: 401,
+      message: "No current password provided",
+    });
+  }
 
-    // Email update request meets email parameters
-    const emailReg = /^\S+@\S+\.\S+$/;
-    if ("email" in req.body == true && emailReg.test(req.body.email) == false) {
-      return Promise.resolve({
-        status: 403,
-        message: "Email provided does not meet email format requirements",
-      });
-    }
+  // Email update request meets email parameters
+  const emailReg = /^\S+@\S+\.\S+$/;
+  if ("email" in req.body == true && emailReg.test(req.body.email) == false) {
+    return Promise.resolve({
+      status: 403,
+      message: "Email provided does not meet email format requirements",
+    });
+  }
 
-    // Updates the user
-    try {
-      const result = await databasePrisma.user.update({
-        where: { id },
-        data: req.body,
-      });
-      result.response = idMsg;
-      delete result.password;
-      delete result.salt;
-      return result;
-    } catch (error) {
-      if (!error.status) {
-        // Checks for database related errors
-        if (error.meta != undefined) {
-          return Promise.resolve({
-            status: 409,
-            message:
-              "The unique input ${error.meta.target[0]} already exists for another user",
-          });
-        } else {
-          return Promise.resolve({
-            status: 400,
-            message: `An argument or input value does not exist or cannot be edited in the database ${error.message}`,
-          });
-        }
+  // Updates the user
+  try {
+    const result = await databasePrisma.user.update({
+      where: { id },
+      data: req.body,
+    });
+    result.response = idMsg;
+    delete result.password;
+    delete result.salt;
+    return result;
+  } catch (error) {
+    if (!error.status) {
+      // Checks for database related errors
+      if (error.meta != undefined) {
+        return Promise.resolve({
+          status: 409,
+          message:
+            "The unique input ${error.meta.target[0]} already exists for another user",
+        });
       } else {
-        if (error.status) {
-          return Promise.resolve({
-            status: error.status,
-            message: error.message,
-          });
-        }
+        return Promise.resolve({
+          status: 400,
+          message: `An argument or input value does not exist or cannot be edited in the database ${error.message}`,
+        });
+      }
+    } else {
+      if (error.status) {
+        return Promise.resolve({
+          status: error.status,
+          message: error.message,
+        });
       }
     }
   }
