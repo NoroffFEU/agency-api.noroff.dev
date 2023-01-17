5771171043e665b7de6e9389d1440c0f5a2403fc
Liridona <91525128+barlaek@users.noreply.github.com>
2023-01-17 14:47:02 +0100

    boilerplate for update.js

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -4,32 +4,32 @@ import { databasePrisma } from "../../prismaClient.js";
 export const offersRouter = express.Router();
 
 // Handling request using router
-offersRouter.put("/:id", async (req, res) => {
-  const { id } = req.params;
+// offersRouter.put("/:id", async (req, res) => {
+//   const { id } = req.params;
 
-  const {
-    listingId,
-    applicationId,
-    userId,
-    applicantId,
-    userRole,
-    offerState,
-  } = req.body;
+//   const {
+//     listingId,
+//     applicationId,
+//     userId,
+//     applicantId,
+//     userRole,
+//     offerState,
+//   } = req.body;
 
-  try {
-    const offer = await databasePrisma.offer.update({
-      where: {
-        id: id,
-      },
-      data: {
-        id: id,
-        ...req.body,
-      },
-    });
+//   try {
+//     const offer = await databasePrisma.offer.update({
+//       where: {
+//         id: id,
+//       },
+//       data: {
+//         id: id,
+//         ...req.body,
+//       },
+//     });
 
-    res.status(200).json(offer);
-  } catch (error) {
-    console.log(error);
-    res.status(400);
-  }
-});
+//     res.status(200).json(offer);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400);
+//   }
+// });
--- src/modules/offer/update.js
+++ src/modules/offer/update.js
@@ -1,10 +1,35 @@
+import { OfferState, prisma } from "@prisma/client";
 import express from "express";
 import { databasePrisma } from "../../prismaClient";
 
 export const putOffers = express.Router();
 
-putOffers.put("/:id", (req, res) => {
+putOffers.put("/:id", async (req, res) => {
   const { id } = req.params;
 
-  res.send();
+  const {
+    listingId,
+    applicationId,
+    companyId,
+    userId,
+    applicantId,
+    state,
+    updated,
+  } = req.body;
+
+  try {
+    const updateOffer = await prisma.offer.update({
+      where: {
+        id: id,
+      },
+      data: {
+        id: id,
+        ...req.body,
+      },
+    });
+    res.status(200).json(updateOffer);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
 });
