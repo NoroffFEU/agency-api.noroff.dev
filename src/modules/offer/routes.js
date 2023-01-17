e32f74c9451d5c77d99a1283522ccbcea4b71c5a
Liridona <91525128+barlaek@users.noreply.github.com>
2023-01-17 14:50:16 +0100

    cleanup in router.js @offer

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -6,30 +6,3 @@ export const offersRouter = express.Router();
 // Handling request using router
 // offersRouter.put("/:id", async (req, res) => {
 //   const { id } = req.params;
-
-//   const {
-//     listingId,
-//     applicationId,
-//     userId,
-//     applicantId,
-//     userRole,
-//     offerState,
-//   } = req.body;
-
-//   try {
-//     const offer = await databasePrisma.offer.update({
-//       where: {
-//         id: id,
-//       },
-//       data: {
-//         id: id,
-//         ...req.body,
-//       },
-//     });
-
-//     res.status(200).json(offer);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400);
-//   }
-// });
