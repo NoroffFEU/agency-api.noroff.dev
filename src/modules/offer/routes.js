b42c0f5414b265e01b017a7b1a93bfdf437df612
pnordboj <patrick@custompublish.com>
2023-01-18 11:15:16 +0100

    Added read.js and made the new changes

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -1,28 +1,8 @@
 import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
+import { offers, offer } from "./read.js";
 
 export const offersRouter = express.Router();
 
-// Handling request using router
-offersRouter.get("/", async (req, res) => {
-  res.send("This is the offers request");
-});
-
-offersRouter.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const offer = await databasePrisma.offer.findUnique({
-      where: {
-        id: String(id),
-      },
-    });
-    if (!offer) {
-      res.status(404).json({ message: "Offer not found" });
-      return;
-    }
-    res.send(offer);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: `${error}` });
-  }
-});
+offersRouter.get("/", offers);
+offersRouter.get("/:id", offer);
