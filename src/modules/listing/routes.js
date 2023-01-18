f686b5727e59a07e3e0a14fc674d88e29e007a9c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-01-18 21:07:59 +0100

    adds put function

--- src/modules/listing/routes.js
+++ src/modules/listing/routes.js
@@ -3,125 +3,10 @@ import { databasePrisma } from "../../prismaClient.js";
 
 export const listingsRouter = express.Router();
 
-// Handling request using router
-// GET /listings
-listingsRouter
-  .get("/", async (req, res) => {
-    const isActive = JSON.stringify(req.query.isActive);
-
-    try {
-      // Get listings from database
-
-      if (isActive) {
-        if (JSON.parse(isActive) === "true") {
-          console.log("it's true");
-          const listings = await databasePrisma.listing.findMany({
-            where: {
-              listingsState: "Active",
-            },
-          });
-          console.log(listings);
-          res.status(200).json(listings);
-        } else if (JSON.parse(isActive) === "false") {
-          console.log("it's false");
-          const listings = await databasePrisma.listing.findMany({
-            where: {
-              listingsState: "Ended",
-            },
-          });
-          console.log(listings);
-          res.status(200).json(listings);
-        } else {
-          console.log("invalid query");
-        }
-      }
-
-      const listings = await databasePrisma.listing.findMany();
-      // listings.map((item) => {
-      //   console.log(item.listingsState);
-      // });
-
-      // Show the listings in browser
-      res.status(200).json(listings);
-    } catch (error) {
-      // Show error message in browser
-      res
-        .status(500)
-        .json({ message: `internal server error`, statusCode: "500" });
-    }
-  })
-  .get("/:id", async (req, res, next) => {
-    try {
-      // Get id from url
-      const urlID = req.params.id;
-
-      const uniqueListing = await databasePrisma.listing.findUnique({
-        where: {
-          id: urlID,
-        },
-      });
-
-      // Check to see if array is empty
-      if (uniqueListing === null) {
-        // res.status(404).json({ message: `There are no listings with an id of '${urlID}'` });
-        return res.status(404).json({
-          message: `Listing with id: ${req.params.id} doesn't exist.`,
-        });
-      }
-
-      res.status(200).json(uniqueListing);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `internal server error`, statusCode: "500" });
-    }
-  });
-
-// POST /listings
-listingsRouter.post("/", async (req, res) => {
-  try {
-    const { title, tags, description, requirements, deadline, authorId } =
-      req.body;
-
-    const now = new Date().getTime();
-    const valid = now < new Date(deadline).getTime();
-
-    if (!valid) {
-      res
-        .status(400)
-        .json({ message: "Deadline must be greater than todays date" });
-    } else if (
-      title &&
-      tags &&
-      description &&
-      requirements &&
-      deadline &&
-      authorId
-    ) {
-      const result = await databasePrisma.listing.create({
-        data: {
-          title: title,
-          tags: tags,
-          description: description,
-          requirements: requirements,
-          deadline: deadline,
-          authorId: authorId,
-        },
-      });
-
-      res.status(201).json(result);
-    } else {
-      res.status(400).json({ message: "Please fill in all required fields" });
-    }
-  } catch (e) {
-    res.status(500).json({ message: `${e}` });
-  }
-});
-
 // PUT /listings/:id
 listingsRouter.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const { deadline, editId } = req.body;
+  const { deadline } = req.body;
 
   const now = new Date().getTime();
   const newDeadline = new Date(deadline).getTime();
@@ -134,7 +19,6 @@ listingsRouter.put("/:id", async (req, res) => {
       message: "deadline must be a future date",
     });
   }
-
   try {
     const listings = await databasePrisma.listing.update({
       where: {
@@ -145,63 +29,15 @@ listingsRouter.put("/:id", async (req, res) => {
         ...req.body,
       },
     });
-    if (editId === listings.authorId) {
-      res.status(200).json({
-        data: {
-          listings,
-        },
-      });
-    } else {
-      res.status(400).json({
-        message: "you can not edit other users listings",
-      });
-    }
+
+    res.status(200).json({
+      data: {
+        listings,
+      },
+      message: `Listing updated successfully`,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
     throw new Error(error, "An error occurred in listingsRouter.put()");
   }
 });
-
-// DELETE /listings/:id
-listingsRouter.delete("/:id", getListing, async (req, res) => {
-  try {
-    await databasePrisma.listing.delete({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res
-      .status(200)
-      .json({ message: `Listing with id: ${req.params.id} was deleted.` });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-/**
- * Middleware function that checks if listing with specified id does exist.
- * @param {*} req
- * @param {*} res
- * @param {*} next
- * @returns "status 404" with error message if listing doesn't exist.
- */
-async function getListing(req, res, next) {
-  let listing;
-  try {
-    listing = await databasePrisma.listing.findUnique({
-      where: {
-        id: req.params.id,
-      },
-    });
-    next();
-    if (listing == null) {
-      return res
-        .status(404)
-        .json({ message: `Listing with id: ${req.params.id} doesn't exist.` });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-
-  res.listing = listing;
-}
