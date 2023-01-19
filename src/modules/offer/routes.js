23769fa25c6ddf91a5d4008c4975b0b9b77869de
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-01-19 17:14:29 +0100

    imported and called  create and remove offer in routes.js

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -1,8 +1,18 @@
 import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
 import { offers, offer } from "./read.js";
+import { createOffer } from "./create.js";
+import { removeOffer } from "./delete.js";
 
 export const offersRouter = express.Router();
 
 offersRouter.get("/", offers);
 offersRouter.get("/:id", offer);
+
+offersRouter.post("/", async (req, res) => {
+  createOffer(databasePrisma, req, res);
+});
+
+offersRouter.delete("/:id", async (req, res) => {
+  removeOffer(databasePrisma, req, res);
+});
