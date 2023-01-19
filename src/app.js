b430bcda9e0ecacbcd4d0b2d6883f41e305cc1f9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-01-19 11:25:08 +0100

    Added options for swagger JSDocs

--- src/app.js
+++ src/app.js
@@ -5,10 +5,42 @@ import { listingsRouter } from "./modules/listing/routes.js";
 import { offersRouter } from "./modules/offer/routes.js";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+// options for swagger jsdocs
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Noroff Agency API",
+      version: "0.0.1",
+      description:
+        "An Express and Prisma rest API to server a Noroff students and industry partners in finding jobs and placements.",
+      license: {
+        name: "Licensed Under MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "apiKey",
+          name: "Authorization",
+          scheme: "bearer",
+          in: "header",
+        },
+      },
+    },
+  },
+  apis: ["./src/modules/**/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(
   bodyParser.json({
@@ -21,4 +53,6 @@ app.use("/applications", applicationsRouter);
 app.use("/listings", listingsRouter);
 app.use("/offers", offersRouter);
 
-const server = app.listen(PORT, () => console.log(`🚀 Server ready at: http://localhost:${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`🚀 Server ready at: http://localhost:${PORT}`)
+);
