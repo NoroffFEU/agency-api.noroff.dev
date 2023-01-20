f73162a6bb5475c0847af27a7edb8c87a989b503
Eric Pretzinger <eric.pretzinger@kabal.com>
2023-01-20 08:43:53 +0100

    Fix spelling errors

--- src/modules/application/routes.js
+++ src/modules/application/routes.js
@@ -7,62 +7,64 @@ import { handleCreate } from "./controllers/controllerCreate.js";
 export const applicationsRouter = express.Router();
 
 applicationsRouter
-    .get("/", checkAuth, async (req, res) => {
-        try {
-            const applications = await databasePrisma.application.findMany({
-                include: { offers: true },
-            });
-            res.status(200).json(applications);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: `internal server error`, code: "500" });
-        }
-    })
-    .get("/:id", checkAuth, async (req, res) => {
-        try {
-            const params = req.query;
-            const URLid = req.params.id;
-            const applicationById = await databasePrisma.application.findUnique({
-                where: {
-                    id: URLid,
-                },
-                include: { _count: true, ...params },
-            });
-            if (applicationById === null) {
-                res.status(404).json({ message: `no application found with id "${URLid}"` });
-                return;
-            }
-            res.json(applicationById);
-        } catch (err) {
-            if (err instanceof Prisma.PrismaClientValidationError) {
-                res.status(400).send("mispelled url param");
-            } else {
-                res.status(500).send(`internal server error `);
-            }
-        }
-    })
-    .post("/", checkAuth, async (req, res) => {
-        try {
-            //NEEDS MORE TESTING
-            const result = await handleCreate(req, res);
-            res.status(200).json(result);
-        } catch (err) {
-            res.status(400).json({ message: `${err}`, code: "400" });
-        }
-    })
-    .delete("/:id", checkAuth, async (req, res) => {
-        try {
-            const id = req.params.id;
-            const result = await databasePrisma.application.delete({
-                where: {
-                    id: id,
-                },
-            });
-            res.status(200).json({
-                message: "Succefully deleted application with id: " + id,
-                code: "200",
-            });
-        } catch (err) {
-            res.status(400).json({ message: `${err}`, code: "400" });
-        }
-    });
+  .get("/", checkAuth, async (req, res) => {
+    try {
+      const applications = await databasePrisma.application.findMany({
+        include: { offers: true },
+      });
+      res.status(200).json(applications);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: `internal server error`, code: "500" });
+    }
+  })
+  .get("/:id", checkAuth, async (req, res) => {
+    try {
+      const params = req.query;
+      const URLid = req.params.id;
+      const applicationById = await databasePrisma.application.findUnique({
+        where: {
+          id: URLid,
+        },
+        include: { _count: true, ...params },
+      });
+      if (applicationById === null) {
+        res
+          .status(404)
+          .json({ message: `no application found with id "${URLid}"` });
+        return;
+      }
+      res.json(applicationById);
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientValidationError) {
+        res.status(400).send("misspelled url param");
+      } else {
+        res.status(500).send(`internal server error `);
+      }
+    }
+  })
+  .post("/", checkAuth, async (req, res) => {
+    try {
+      //NEEDS MORE TESTING
+      const result = await handleCreate(req, res);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(400).json({ message: `${err}`, code: "400" });
+    }
+  })
+  .delete("/:id", checkAuth, async (req, res) => {
+    try {
+      const id = req.params.id;
+      await databasePrisma.application.delete({
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).json({
+        message: "Successfully deleted application with id: " + id,
+        code: "200",
+      });
+    } catch (err) {
+      res.status(400).json({ message: `${err}`, code: "400" });
+    }
+  });
