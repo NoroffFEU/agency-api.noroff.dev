8a01a002f9caf2119001f6aba36b52d976e8fe37
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-01-20 11:32:54 +0100

    updated my branch with new items from application branch

--- src/modules/application/controllers/controllerCreate.js
+++ src/modules/application/controllers/controllerCreate.js
@@ -1,26 +1,26 @@
 import { databasePrisma } from "../../../prismaClient.js";
 
 export const handleCreate = async function (req, res) {
-    const data = req.body;
-    try {
-        const result = await databasePrisma.application.create({
-            data: data,
-        });
+  const data = req.body;
+  try {
+    const result = await databasePrisma.application.create({
+      data: data,
+    });
 
-        return result;
-    } catch (error) {
-        if (error.code === "P2002") {
-            return Promise.resolve({
-                status: 409,
-                message: `You've already created an application on this listing`,
-            });
-        } else if (error.status) {
-            return Promise.resolve({
-                status: error.status,
-                message: error.message,
-            });
-        }
-
-        console.log(error);
+    return result;
+  } catch (error) {
+    if (error.code === "P2002") {
+      return Promise.resolve({
+        status: 409,
+        message: `You've already created an application on this listing`,
+      });
+    } else if (error.status) {
+      return Promise.resolve({
+        status: error.status,
+        message: error.message,
+      });
     }
+
+    console.log(error);
+  }
 };
