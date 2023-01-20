2a387d79f68814c9cd5af2ec8098caaab6357f4a
Liridona <91525128+barlaek@users.noreply.github.com>
2023-01-20 14:35:51 +0100

    import correct updateOffer function

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -3,7 +3,7 @@ import { databasePrisma } from "../../prismaClient.js";
 import { offersGet, offerGetId } from "./read.js";
 import { createOffer } from "./create.js";
 import { removeOffer } from "./delete.js";
-import { offerPut } from "./update.js";
+import { updateOffer } from "./update.js";
 
 export const offersRouter = express.Router();
 
@@ -13,14 +13,14 @@ offersRouter.get("/", async (req, res) => {
 
 offersRouter.get("/:id", async (req, res) => {
   offerGetId(databasePrisma, req, res);
- });
+});
 
 offersRouter.post("/", async (req, res) => {
   createOffer(databasePrisma, req, res);
 });
 
 offersRouter.put("/:id", async (req, res) => {
-  offerPut(databasePrisma, req, res);
+  updateOffer(databasePrisma, req, res);
 });
 
 offersRouter.delete("/:id", async (req, res) => {
