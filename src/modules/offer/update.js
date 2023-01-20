4229664968540586f75f1be41f2cb0e0f8ad9d45
Liridona <91525128+barlaek@users.noreply.github.com>
2023-01-20 10:54:26 +0100

    update function cleanup

--- src/modules/offer/update.js
+++ src/modules/offer/update.js
@@ -28,7 +28,7 @@ export const offerPut = putOffers.put("/:id", async (req, res) => {
         ...req.body,
       },
     });
-    res.status(200).json(updateOffer);
+    res.status(200).json(offerPut);
   } catch (error) {
     console.log(error);
     res.status(400);
