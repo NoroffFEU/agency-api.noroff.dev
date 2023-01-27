7ee17ba4160d9db71f71583f9d9b5e59ff63a4be
AdrianFred <adrian989@me.com>
2023-01-27 22:56:25 +0100

    Changing routes to see many/all companies,  & post

--- src/modules/company/routes.js
+++ src/modules/company/routes.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { databasePrisma } from "../../prismaClient.js";
+import { createCompany } from "./controllers/createCompany.js";
 
 export const companyRouter = express.Router();
 
 // Handling request using router
+// companyRouter.get("/", async (req, res) => {
+//   res.send("This is the company request");
+// });
+
+companyRouter.get("/", async (req, res) => {
+  databasePrisma.company.findMany().then((companies) => {
+    res.send(companies);
+  });
+});
+
 companyRouter.post("/", async (req, res) => {
-  res.send("This is the company request");
+  createCompany(databasePrisma, req, res);
 });
 
 companyRouter.put("/", async (req, res) => {});
