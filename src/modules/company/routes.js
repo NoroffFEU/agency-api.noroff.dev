4fcf7e900e0a8d9ca68b095d034c84d47e36dfd7
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-27 20:28:30 +0100

    added some company endpoints to fill

--- src/modules/company/routes.js
+++ src/modules/company/routes.js
@@ -4,6 +4,14 @@ import { databasePrisma } from "../../prismaClient.js";
 export const companyRouter = express.Router();
 
 // Handling request using router
-companyRouter.get("/", async (req, res) => {
+companyRouter.post("/", async (req, res) => {
   res.send("This is the company request");
 });
+
+companyRouter.put("/", async (req, res) => {});
+
+companyRouter.get("/", async (req, res) => {});
+
+companyRouter.get("/:id", async (req, res) => {});
+
+companyRouter.delete("/", async (req, res) => {});
