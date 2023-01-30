0c433dc26e8dd4846733b61b3131b94c735aa23f
AdrianFred <adrian989@me.com>
2023-01-30 01:30:28 +0100

    Added a route to get all companies

--- src/modules/company/routes.js
+++ src/modules/company/routes.js
@@ -3,24 +3,14 @@ import { databasePrisma } from "../../prismaClient.js";
 import { createCompany } from "./controllers/createCompany.js";
 import { findCompanyById } from "./controllers/findCompanyById.js";
 import { deleteCompany } from "./controllers/deleteCompany.js";
+import { findAllCompanies } from "./controllers/findAllCompanies.js";
 
 export const companyRouter = express.Router();
 
 // Endpoint to get all companies in a list
 
 companyRouter.get("/list", async (req, res) => {
-  databasePrisma.company
-    .findMany({
-      include: {
-        applications: true,
-        offers: true,
-        listings: true,
-        admin: true,
-      },
-    })
-    .then((companies) => {
-      res.send(companies);
-    });
+  findAllCompanies(databasePrisma, req, res);
 });
 
 // Endpoint to get a company by id
