1e80629fa6594ecce2914fbf96a75273273db462
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-02-01 21:46:54 +0100

    Added some date processing to update listing

--- src/modules/listing/controllers/controllerUpdate.js
+++ src/modules/listing/controllers/controllerUpdate.js
@@ -3,27 +3,51 @@ import { databasePrisma } from "../../../prismaClient.js";
 export const updateListing = async function (req, res) {
   try {
     const { id } = req.params;
-    const { deadline } = req.body;
+    const { title, tags, description, requirements, deadline, company } =
+      req.body;
+    //check all required fields have been added
+    const data = { title, tags, description, requirements, deadline };
+    let errors = [];
+    ["title", "tags", "description", "requirements", "deadline"].forEach(
+      (key) => {
+        if (data[key] === undefined) {
+          delete data[key];
+        }
+      }
+    );
 
-    const now = new Date().getTime();
-    const newDeadline = new Date(deadline).getTime();
-    const valid = now < newDeadline;
+    // check for valid date
+    if (deadline !== undefined) {
+      const now = new Date().getTime();
+      const date = new Date(deadline).getTime();
+      if (isNaN(date)) {
+        return res
+          .status(400)
+          .json({ message: "Deadline must be in a valid ISO date format." });
+      }
+      // check for future date
+      const valid = now < date;
+      if (!valid) {
+        return res
+          .status(400)
+          .json({ message: "Deadline must be greater than todays date" });
+      }
 
-    if (!deadline) {
-      res.status(400).json({ message: "deadline is required" });
-    } else if (deadline !== undefined && !valid) {
-      res.status(400).json({
-        message: "deadline must be a future date",
-      });
+      if (!deadline) {
+        res.status(400).json({ message: "deadline is required" });
+      } else if (deadline !== undefined && !valid) {
+        res.status(400).json({
+          message: "deadline must be a future date",
+        });
+      }
+      data.deadline = new Date(deadline).toISOString();
     }
+
     const listings = await databasePrisma.listing.update({
       where: {
         id: id,
       },
-      data: {
-        id: id,
-        ...req.body,
-      },
+      data,
       include: { company: true },
     });
 
