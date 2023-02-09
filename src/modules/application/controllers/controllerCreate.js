7ef73d277d46e07c4622edda0343dcae3bb9e247
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-02-09 19:18:46 +0100

    checks for existing application

--- src/modules/application/controllers/controllerCreate.js
+++ src/modules/application/controllers/controllerCreate.js
@@ -1,26 +1,45 @@
 import { databasePrisma } from "../../../prismaClient.js";
 
-export const handleCreate = async function (req, res) {
-  const data = req.body;
+export const handleCreate = async function (req) {
+  const { applicantId, listingId, coverLetter, companyId } = req.body;
+
+  const hasUserCreatedApplication = await databasePrisma.application.count({
+    where: {
+      applicantId,
+      listingId,
+    },
+  });
+
+  if (hasUserCreatedApplication > 0) {
+    return Promise.resolve({
+      status: 409,
+      message: "You've already created an application on this listing",
+    });
+  }
+
   try {
     const result = await databasePrisma.application.create({
-      data: data,
+      data: {
+        applicantId,
+        listingId,
+        coverLetter,
+        companyId,
+      },
     });
 
     return result;
   } catch (error) {
-    if (error.code === "P2002") {
-      return Promise.resolve({
-        status: 409,
-        message: `You've already created an application on this listing`,
-      });
-    } else if (error.status) {
+    if (error.status) {
       return Promise.resolve({
         status: error.status,
         message: error.message,
       });
+    } else {
+      return Promise.resolve({
+        status: 400,
+        message:
+          "application ID, listing ID, cover letter, and company ID are required",
+      });
     }
-
-    console.log(error);
   }
 };
