6bd3a8123b6251fa021ec6b45eafe0c4549c1c10
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-02-18 22:31:31 +0100

    additional tests added for users route

--- src/modules/user/routes.test.js
+++ src/modules/user/routes.test.js
@@ -1,6 +1,8 @@
 import request from "supertest";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { databasePrisma } from "../../prismaClient.js";
+import { signToken } from "../../utilities/jsonWebToken.js";
+import { generateHash } from "../../utilities/password.js";
 
 dotenv.config();
 
@@ -14,27 +16,26 @@ const testUser = {
   password: "strongPassword",
 };
 
-const secondTestUser = {
-  firstName: "Kari",
-  lastName: "Nordmann",
-  email: "kari@email.com",
-  password: "olaolaolaola",
+const testUser2 = {
+  firstName: "Jane",
+  lastName: "Doe",
+  email: "someotheremail@email.com",
+  password: "strongPassword",
 };
 
-let createdUser;
-let secondCreatedUser;
-
-let loggedInUser;
-let secondLoggedInUser;
+let firstUser;
+let signedToken1;
+let signedToken2;
+let secondUser;
 
 describe("POST /users", () => {
   beforeAll(async () => {
-    const createdUser = await databasePrisma.user.findUnique({
+    const firstUser = await databasePrisma.user.findUnique({
       where: {
         email: testUser.email,
       },
     });
-    if (createdUser) {
+    if (firstUser) {
       await databasePrisma.user.delete({
         where: {
           email: testUser.email,
@@ -68,32 +69,28 @@ describe("POST /users", () => {
   });
 
   it("should return 400 response with no first name", async () => {
-    const response = await request(baseURL)
-      .post("/users")
-      .send({
-        email: testUser.email,
-        lastName: testUser.lastName,
-        password: testUser.password,
-      });
+    const response = await request(baseURL).post("/users").send({
+      email: testUser.email,
+      lastName: testUser.lastName,
+      password: testUser.password,
+    });
     expect(response.body.message).toEqual("Missing required field, firstName.");
     expect(response.statusCode).toBe(400);
   });
 
   it("should return 400 response with no last name", async () => {
-    const response = await request(baseURL)
-      .post("/users")
-      .send({
-        email: testUser.email,
-        firstName: testUser.firstName,
-        password: testUser.password,
-      });
+    const response = await request(baseURL).post("/users").send({
+      email: testUser.email,
+      firstName: testUser.firstName,
+      password: testUser.password,
+    });
     expect(response.body.message).toEqual("Missing required field, lastName.");
     expect(response.statusCode).toBe(400);
   });
 
   it("should create and return new user object and a 201 response", async () => {
     const response = await request(baseURL).post("/users").send(testUser);
-    createdUser = response.body;
+    firstUser = response.body;
     expect(response.body.email).toEqual(testUser.email);
     expect(response.body.lastName).toEqual(testUser.lastName);
     expect(response.statusCode).toBe(201);
@@ -107,13 +104,25 @@ describe("POST /users", () => {
 });
 
 describe("POST /users/login", () => {
+  beforeAll(async () => {
+    const firstUser = await databasePrisma.user.findUnique({
+      where: {
+        email: testUser.email,
+      },
+    });
+    if (!firstUser) {
+      await databasePrisma.user.create({
+        data: { ...testUser, password: generateHash(testUser.password) },
+      });
+    }
+  });
+
   it("should return an object with user details and a 200 response when successfully logged in", async () => {
     const response = await request(baseURL)
       .post("/users/login")
       .send({ email: testUser.email, password: testUser.password });
-    loggedInUser = response.body;
-    expect(response.body.email).toEqual(loggedInUser.email);
-    expect(response.body.lastName).toEqual(loggedInUser.lastName);
+    expect(response.body.email).toEqual(testUser.email);
+    expect(response.body.lastName).toEqual(testUser.lastName);
     expect(response.statusCode).toBe(200);
   });
 
@@ -121,6 +130,19 @@ describe("POST /users/login", () => {
     const response = await request(baseURL)
       .post("/users/login")
       .send({ email: testUser.email, password: "badPassword" });
+    expect(response.body.email).toEqual(undefined);
+    expect(response.body.lastName).toEqual(undefined);
+    expect(response.body.message).toEqual("Invalid email or password.");
+    expect(response.statusCode).toBe(403);
+  });
+
+  it("should not return user details if incorrect email is provided, and also return a 403 response", async () => {
+    const response = await request(baseURL)
+      .post("/users/login")
+      .send({
+        email: "badtesting313212@something.com",
+        password: "badPassword",
+      });
     expect(response.body.email).toEqual(undefined);
     expect(response.body.lastName).toEqual(undefined);
     expect(response.body.message).toEqual("Invalid email or password.");
@@ -137,64 +159,188 @@ describe("GET /users", () => {
 });
 
 describe("GET /users/:id", () => {
+  beforeAll(async () => {
+    secondUser = await databasePrisma.user.findUnique({
+      where: {
+        email: testUser2.email,
+      },
+    });
+    if (!secondUser) {
+      secondUser = await databasePrisma.user.create({
+        data: {
+          ...testUser2,
+          password: await generateHash(testUser2.password),
+        },
+      });
+    }
+    signedToken2 = signToken(secondUser);
+  });
+
   it("should return a user object and a 200 response", async () => {
-    const { id } = createdUser;
+    const { id } = secondUser;
+
     const response = await request(baseURL).get(`/users/${id}`);
-    expect(response.body.name).toEqual(testUser.name);
+    expect(response.body.name).toEqual(testUser2.name);
+    expect(response.body.password).toEqual(undefined);
+    expect(response.body.offers).toEqual(undefined);
+    expect(response.body.applications).toEqual(undefined);
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("With auth on own user id should return offers and applications", async () => {
+    const { id } = secondUser;
+    const response = await request(baseURL)
+      .get(`/users/${id}`)
+      .set("Authorization", `Bearer ${signedToken2}`);
+    expect(response.body.name).toEqual(testUser2.name);
+    expect(response.body.password).toEqual(undefined);
+    expect(response.body.offers).toEqual(expect.any(Array));
+    expect(response.body.applications).toEqual(expect.any(Array));
     expect(response.statusCode).toBe(200);
   });
 });
 
 describe("PUT /users/:id", () => {
-  it("should return a updated user object and a 200 response", async () => {
-    const { id } = createdUser;
+  beforeAll(async () => {
+    secondUser = await databasePrisma.user.findUnique({
+      where: {
+        email: testUser2.email,
+      },
+    });
+
+    if (!secondUser) {
+      secondUser = await databasePrisma.user.create({
+        data: {
+          ...testUser2,
+          password: await generateHash(testUser2.password),
+        },
+      });
+    } else {
+      await databasePrisma.user.update({
+        where: { email: testUser2.email },
+        data: {
+          firstName: testUser2.firstName,
+          lastName: testUser2.lastName,
+          password: await generateHash("strongPassword"),
+        },
+      });
+    }
+
+    signedToken2 = signToken(secondUser);
+  });
+  it("should return 401 response, with wrong password", async () => {
+    const { id } = secondUser;
     const data = {
-      firstName: "Lesslie",
       password: "newPassword",
+      currentpassword: "strongPass",
+    };
+    const response = await request(baseURL)
+      .put(`/users/${id}`)
+      .set("Authorization", `Bearer ${signedToken2}`)
+      .send(data);
+    console.log(response.body);
+    expect(response.body.message).toEqual("Incorrect Password.");
+    expect(response.statusCode).toBe(401);
+  });
+
+  it("should return 400 response, with short new password", async () => {
+    const { id } = secondUser;
+    const data = {
+      password: "new",
       currentpassword: "strongPassword",
     };
     const response = await request(baseURL)
       .put(`/users/${id}`)
-      .set("Authorization", `Bearer ${loggedInUser.token}`)
+      .set("Authorization", `Bearer ${signedToken2}`)
       .send(data);
-    expect(response.body.firstName).toEqual(data.firstName);
+    console.log(response.body);
+    expect(response.body.message).toEqual(
+      "Password does not meet required parameters length: min 5, max 20."
+    );
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return a updated user object and a 200 response", async () => {
+    const { id } = secondUser;
+    const data = {
+      title: "Queen",
+      firstName: "Leslie",
+      lastName: "Lie",
+      about: "It's about me.",
+      skills: "html,css",
+      password: "newPassword",
+      currentpassword: "strongPassword",
+    };
+
+    const response = await request(baseURL)
+      .put(`/users/${id}`)
+      .set("Authorization", `Bearer ${signedToken2}`)
+      .send(data);
+    console.log(response.body);
+    expect(response.body.title).toEqual("Queen");
+    expect(response.body.firstName).toEqual("Leslie");
+    expect(response.body.lastName).toEqual("Lie");
+    expect(response.body.about).toEqual("It's about me.");
+    expect(response.body.skills).toEqual(["html", "css"]);
     expect(response.statusCode).toBe(200);
   });
 });
 
 describe("DELETE /users/:id", () => {
   beforeAll(async () => {
-    //Creating and login in a second user in order to mix properly created tokens
-    const createUser = await request(baseURL)
-      .post("/users")
-      .send(secondTestUser);
-    const loginUser = await request(baseURL)
-      .post("/users/login")
-      .send({ email: secondTestUser.email, password: secondTestUser.password });
+    secondUser = await databasePrisma.user.findUnique({
+      where: {
+        email: testUser2.email,
+      },
+    });
+    if (!secondUser) {
+      secondUser = await databasePrisma.user.create({
+        data: {
+          ...testUser2,
+          password: await generateHash(testUser2.password),
+        },
+      });
+    }
 
-    secondCreatedUser = createUser.body;
-    secondLoggedInUser = loginUser.body;
-  });
+    firstUser = await databasePrisma.user.findUnique({
+      where: {
+        email: testUser.email,
+      },
+    });
+    if (!firstUser) {
+      await databasePrisma.user.create({
+        data: testUser,
+      });
+    }
 
-  it("should return status 200", async () => {
-    const { id } = createdUser;
-    const response = await request(baseURL)
-      .delete(`/users/${id}`)
-      .set("Authorization", `Bearer ${loggedInUser.token}`);
-    expect(response.statusCode).toBe(200);
-  });
-  it("should return a status 401 when provided with an other users token", async () => {
-    const { id } = secondCreatedUser;
-    const response = await request(baseURL)
-      .delete(`/users/${id}`)
-      .set("Authorization", `Bearer ${loggedInUser.token}`);
-    expect(secondLoggedInUser.token).not.toEqual(loggedInUser.token);
-    expect(response.statusCode).toEqual(401);
+    signedToken2 = signToken(secondUser);
+    signedToken1 = signToken(firstUser);
   });
 
   it("should return a status 401 when not provided with a token", async () => {
-    const { id } = secondCreatedUser;
+    const { id } = firstUser;
     const response = await request(baseURL).delete(`/users/${id}`);
+    expect(response.body.message).toEqual("No authorization header provided.");
     expect(response.statusCode).toEqual(401);
+  });
+
+  it("should return a status 401 when provided with an other users token", async () => {
+    const { id } = firstUser;
+    const response = await request(baseURL)
+      .delete(`/users/${id}`)
+      .set("Authorization", `Bearer ${signedToken2}`);
+    expect(response.body.message).toEqual(
+      "You can not edit another users profile."
+    );
+    expect(response.statusCode).toEqual(401);
+  });
+
+  it("should return status 200", async () => {
+    const { id } = firstUser;
+    const response = await request(baseURL)
+      .delete(`/users/${id}`)
+      .set("Authorization", `Bearer ${signedToken1}`);
+    expect(response.body.message).toEqual("User successfully deleted.");
+    expect(response.statusCode).toBe(200);
   });
 });
