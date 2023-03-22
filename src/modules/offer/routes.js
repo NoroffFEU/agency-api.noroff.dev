514dbcc50d9a873dc006e384471039e8c7ef3424
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-22 22:05:03 +0100

    Fix offers route

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -1,37 +1,33 @@
-Offer---PUT---Company
-import express from 'express';
-import { databasePrisma } from '../../prismaClient.js';
-import { offersGet, offerGetId } from './controllers/read.js';
-import { createOffer } from './controllers/create.js';
-import { removeOffer } from './controllers/delete.js';
-import { updateOffer } from './controllers/update.js';
-import { updateCompanyOffer } from './company/update.js';
-
+import express from "express";
+import { databasePrisma } from "../../prismaClient.js";
+import { offersGet, offerGetId } from "./controllers/read.js";
+import { createOffer } from "./controllers/create.js";
+import { removeOffer } from "./controllers/delete.js";
+import { updateOffer } from "./controllers/update.js";
+import { updateCompanyOffer } from "./company/update.js";
 
 export const offersRouter = express.Router();
 
-offersRouter.get('/', async (req, res) => {
-	offersGet(databasePrisma, req, res);
+offersRouter.get("/", async (req, res) => {
+  offersGet(databasePrisma, req, res);
 });
 
-offersRouter.get('/:id', async (req, res) => {
-	offerGetId(databasePrisma, req, res);
+offersRouter.get("/:id", async (req, res) => {
+  offerGetId(databasePrisma, req, res);
 });
 
-offersRouter.post('/', async (req, res) => {
-	createOffer(databasePrisma, req, res);
+offersRouter.post("/", async (req, res) => {
+  createOffer(databasePrisma, req, res);
 });
 
-offersRouter.put('/:id', async (req, res) => {
-	updateOffer(databasePrisma, req, res);
+offersRouter.put("/:id", async (req, res) => {
+  updateOffer(databasePrisma, req, res);
 });
 
-Offer---PUT---Company
-offersRouter.delete('/:id', async (req, res) => {
-	removeOffer(databasePrisma, req, res);
+offersRouter.delete("/:id", async (req, res) => {
+  removeOffer(databasePrisma, req, res);
 });
 
-offersRouter.put('/company/:id', async (req, res) => {
-	updateCompanyOffer(databasePrisma, req, res);
-
+offersRouter.put("/company/:id", async (req, res) => {
+  updateCompanyOffer(databasePrisma, req, res);
 });
