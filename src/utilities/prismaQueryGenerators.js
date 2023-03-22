0c63e6fe441c35d55a52dbbde0b6401fe6e5edd7
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-22 22:33:38 +0100

    added company sorting/filter/pagination

--- src/utilities/prismaQueryGenerators.js
+++ src/utilities/prismaQueryGenerators.js
@@ -1,5 +1,5 @@
 /**
- * Returns a prisma query object, with current page and results limit
+ * Returns a prisma query object, with current page and results limit for listings
  * @param {Object} req object
  * @returns {Object} containing query object, current page, results limit.
  * @example
@@ -30,6 +30,75 @@ export const createPrismaQueryListings = function (req) {
   }
 
   if ((includes === "title", includes === "description")) {
+    // Add filtering for strings
+    if (includes && includesValue) {
+      prismaQuery.where = {
+        [includes]: {
+          contains: includesValue,
+        },
+      };
+    }
+  }
+
+  if (startDate || endDate) {
+    prismaQuery.where = prismaQuery.where || {};
+    prismaQuery.where.createdAt = {};
+
+    if (startDate) {
+      prismaQuery.where.createdAt.gte = new Date(startDate);
+    }
+
+    if (endDate) {
+      prismaQuery.where.createdAt.lte = new Date(endDate);
+    }
+  }
+
+  // Add sorting
+  if (sortBy && orderBy) {
+    prismaQuery.orderBy = {
+      [sortBy]: orderBy,
+    };
+  }
+
+  // Add pagination
+  prismaQuery.skip = offset;
+  prismaQuery.take = limit;
+
+  return { prismaQuery, page, limit };
+};
+
+/**
+ * Returns a prisma query object, with current page and results limit for company
+ * @param {Object} req object
+ * @returns {Object} containing query object, current page, results limit.
+ * @example
+ *const { prismaQuery, page, limit } = createPrismaQueryCompany(req);
+ */
+export const createPrismaQueryCompany = function (req) {
+  const { sortBy, orderBy, includes, includesValue, startDate, endDate } =
+    req.query;
+
+  // Set default values for page and limit if not provided
+  const page = req.query.page ? parseInt(req.query.page) : 1;
+  const limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+  // Calculate the offset for pagination
+  const offset = (page - 1) * limit;
+
+  // Build query object for Prisma
+  const prismaQuery = {};
+  // if ((includes === "tags", includes === "requirements")) {
+  //   // Add filtering for arrays
+  //   if (includes && includesValue) {
+  //     prismaQuery.where = {
+  //       [includes]: {
+  //         has: includesValue,
+  //       },
+  //     };
+  //   }
+  // }
+
+  if ((includes === "name", includes === "sector")) {
     // Add filtering for strings
     if (includes && includesValue) {
       prismaQuery.where = {
