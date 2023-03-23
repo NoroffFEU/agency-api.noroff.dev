6fee3e613cb6cbc82770f61acf267e8d4dc83c0b
Kenn Torg <kennettorgersenmail@gmail.com>
2023-03-23 11:31:10 +0100

    bad imports

--- src/modules/offer/routes.test.js
+++ src/modules/offer/routes.test.js
@@ -1,7 +1,5 @@
 import request from "supertest";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { createOffer } from "./create";
-import { applicationsRouter } from "../application/routes";
 
 dotenv.config();
 
