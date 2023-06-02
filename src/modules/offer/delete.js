7a9d87b19b16089b784ed984493c12bcaac0b965
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-06-02 18:15:51 +0200

    added verification check

--- src/modules/offer/delete.js
+++ src/modules/offer/delete.js
@@ -1,14 +1,18 @@
+import { verifyToken } from "../../utilities/jsonWebToken";
 export async function removeOffer(prismaClient, request, response) {
   const { id } = request.params; //this might need to be moved out of this function and into the offerRouter.delete function in routes.js
-
   try {
     const offer = await prismaClient.offer.delete({
       where: {
         id: parseInt(id),
       },
     });
+
+    let verified = await verifyToken(JWT);
+
     response.status(200);
   } catch (error) {
     response.status(400);
+    console.log(error);
   }
 }
