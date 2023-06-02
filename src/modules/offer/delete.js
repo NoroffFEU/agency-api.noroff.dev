9c04b7282c59768bc656611a0b6b610c7d4d38ac
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-06-02 18:22:30 +0200

    added verification to check for company n admin

--- src/modules/offer/delete.js
+++ src/modules/offer/delete.js
@@ -1,18 +1,27 @@
 import { verifyToken } from "../../utilities/jsonWebToken";
 export async function removeOffer(prismaClient, request, response) {
   const { id } = request.params; //this might need to be moved out of this function and into the offerRouter.delete function in routes.js
-  try {
-    const offer = await prismaClient.offer.delete({
-      where: {
-        id: parseInt(id),
-      },
-    });
-
-    let verified = await verifyToken(JWT);
-
-    response.status(200);
-  } catch (error) {
-    response.status(400);
-    console.log(error);
+  let verified = await verifyToken(JWT);
+  if (verified.companyId === id || verified.role === "Admin") {
+    try {
+      const offer = await prismaClient.offer.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      return response
+        .status(200)
+        .send({ message: "Offer deleted successfully", company });
+    } catch (error) {
+      if (error) {
+        return response
+          .status(409)
+          .send({ message: "Bad request", error: error });
+      } else {
+        return response.status(401).send({
+          message: "User is not authorized to delete offer",
+        });
+      }
+    }
   }
 }
