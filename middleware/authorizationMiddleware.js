9a2ccb1312b1c621cd73d0ef7a1651d049450496
sakonpatsamit <95245596+sakonpatsamit@users.noreply.github.com>
2023-06-10 19:34:20 +0200

    Not sure what I am doing

---
+++ middleware/authorizationMiddleware.js
@@ -0,0 +1,27 @@
+import { databasePrisma } from "../../../prismaClient.js";
+import { verifyToken } from "../../../utilities/jsonWebToken.js";
+
+export const validateApplicantUpdate = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const userInfo = verifyToken(token);
+    const userId = userInfo.id;
+    const isAdmin = userInfo.role === "admin";
+
+    const id = req.params.id;
+    const offer = await databasePrisma.offer.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!isAdmin && offer.userId !== userId) {
+      return res.status(401).json({ error: "Unauthorized access" });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error in validateApplicantUpdate:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
