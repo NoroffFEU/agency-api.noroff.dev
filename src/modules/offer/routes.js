f7754e3f41f2bcb3548a539f07db53d49cd154b3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-09-10 21:53:27 +0200

    moving and adding middleware to offer route

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -1,33 +1,41 @@
-import express from 'express';
-import { databasePrisma } from '../../prismaClient.js';
-import { offersGet, offerGetId } from './controllers/read.js';
-import { createOffer } from './controllers/create.js';
-import { removeOffer } from './controllers/delete.js';
-import { updateOffer } from './controllers/update.js';
-import { updateCompanyOffer } from './company/update.js';
+import express from "express";
+import { databasePrisma } from "../../prismaClient.js";
+import { offersGet, offerGetId } from "./controllers/read.js";
+import { createOffer } from "./controllers/create.js";
+import { removeOffer } from "./controllers/delete.js";
+import { updateOffer } from "./controllers/update.js";
+import { checkUpdate } from "./middleware/checkUpdate.js";
+import { validateAdminUpdate } from "./middleware/validateAdminUpdate.js";
 
 export const offersRouter = express.Router();
 
-offersRouter.get('/', async (req, res) => {
-	offersGet(databasePrisma, req, res);
+offersRouter.get("/", async (req, res) => {
+  offersGet(databasePrisma, req, res);
 });
 
-offersRouter.get('/:id', async (req, res) => {
-	offerGetId(databasePrisma, req, res);
+offersRouter.get("/:id", async (req, res) => {
+  offerGetId(databasePrisma, req, res);
 });
 
-offersRouter.post('/', async (req, res) => {
-	createOffer(databasePrisma, req, res);
+offersRouter.post("/", async (req, res) => {
+  createOffer(databasePrisma, req, res);
 });
 
-offersRouter.put('/:id', async (req, res) => {
-	updateOffer(databasePrisma, req, res);
+//use this when admin is updating offer
+offersRouter.put("/:id", checkUpdate, validateAdminUpdate, async (req, res) => {
+  updateOffer(databasePrisma, req, res);
 });
 
-offersRouter.delete('/:id', async (req, res) => {
-	removeOffer(databasePrisma, req, res);
-});
+offersRouter.delete(
+  "/:id",
+  checkUpdate,
+  validateAdminUpdate,
+  async (req, res) => {
+    removeOffer(databasePrisma, req, res);
+  }
+);
 
-offersRouter.put('/company/:id', async (req, res) => {
-	updateCompanyOffer(databasePrisma, req, res);
+//use this when user is updating offer status
+offersRouter.put("/user/:id", async (req, res) => {
+  updateOffer(databasePrisma, req, res);
 });
