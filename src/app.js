f939fabb9e1625f73d957cb2b349eea389e7b788
RamtinMoshtagh <95631899+RamtinMoshtagh@users.noreply.github.com>
2024-01-18 09:36:17 +0100

    revised 2 lines in .env and app.js causing conflict

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import swaggerJsDoc from "swagger-jsdoc";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // options for swagger jsdocs
 const options = {
