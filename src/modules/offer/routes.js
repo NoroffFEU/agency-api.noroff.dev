1a7ff9ace04aa1fda87a491a5f32f15d4d9d2767
Anclagen <91462616+Anclagen@users.noreply.github.com>
2024-01-19 13:25:17 +0100

    Merged overlapping code into one file

--- src/modules/offer/routes.js
+++ src/modules/offer/routes.js
@@ -6,7 +6,6 @@ import { removeOffer } from "./controllers/delete.js";
 import { updateOffer } from "./controllers/update.js";
 import { updateCompanyOffer } from "./company/update.js";
 import { checkUserIsUserOfOffer } from "./middleware/checkUserIsUserOfOffer.js";
-import { validateApplicantUpdate } from "./middleware/authorizationMiddleware.js";
 
 export const offersRouter = express.Router();
 
@@ -22,14 +21,9 @@ offersRouter.post("/", async (req, res) => {
   createOffer(databasePrisma, req, res);
 });
 
-offersRouter.put(
-  "/:id",
-  checkUserIsUserOfOffer,
-  validateApplicantUpdate,
-  async (req, res) => {
-    updateOffer(databasePrisma, req, res);
-  }
-);
+offersRouter.put("/:id", checkUserIsUserOfOffer, async (req, res) => {
+  updateOffer(databasePrisma, req, res);
+});
 
 offersRouter.delete("/:id", async (req, res) => {
   removeOffer(databasePrisma, req, res);
